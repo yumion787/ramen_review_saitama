c1f7e63e420532b8c5b8232e77014dc1a18bc6ae
takuya.yumisashi <yt.lions.3@gmail.com>
2019-11-19 10:55:41 +0000

    top page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'toppages/index'
+  root to: 'toppages#index'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
